5a131acd91be538618ba1375f70218ec695c40a8
haohuynhvet2020-lab <haohuynhvet2020@gmail.com>
2026-09-11 19:45:46 +0700

    upload code chuẩn

--- ServiceImageSlider.tsx
+++
@@ -1,7 +0,0 @@
-import HandbookClient from "./HandbookClient";
-import { getAllArticles } from "@/lib/articles";
-
-export default function HandbookPage() {
-  const articles = getAllArticles();
-  return <HandbookClient articles={articles} />;
-}
